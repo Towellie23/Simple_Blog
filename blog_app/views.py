2f13c6f6a32de9af24e03a4405c4d0c5d26fd528
Towellie 23 <eduard.matveev.work@gmail.com>
2025-01-27 00:20:58 +0300

    Add comment form and opportunity to create comments

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 
 def post_listing(request):
@@ -9,7 +9,22 @@ def post_listing(request):
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.all()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'comment_form': comment_form, 'comments': comments})
+
+
+    # post = get_object_or_404(Post, id=id)
+    # return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_create(request):
     if request.method == 'POST':
@@ -18,7 +33,7 @@ def post_create(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', id=post.id)
+            return redirect('home')
     else:
         form = PostForm()
     return render(request, 'blog/post_form.html', {'form': form})
